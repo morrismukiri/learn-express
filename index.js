6031a99df389908146e0d9b0eab2d58e8a50a9d0
Morris Mukiri <morrismukiri@gmail.com>
2015-12-22 21:34:42 +0300

    Major Minor

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
+var app=require('./app.js');
+// var = express();
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
