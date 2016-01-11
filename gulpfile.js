db647c4cf2fc9094f77edaf3234288004a7aecd9
Morris Mukiri <morrismukiri@gmail.com>
2016-01-11 15:19:22 +0300

    added gulp nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
-var jscs= require('gulp-jscs');
+var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'routes/*.js', 'views/*.js'];
 
@@ -8,4 +9,25 @@ gulp.task('style', function () {
 	return gulp.src(jsFiles)
 		.pipe(jshint())
 		.pipe(jshint.reporter('jshint-stylish', { verbose: true })).pipe(jscs());
+});
+
+gulp.task('serve', ['style'], function () {
+	var options={
+	script: './bin/www',
+	delayTime:1,
+	env:{
+		port: 8080
+	},	
+	watch:jsFiles
+	};
+	return nodemon(options)
+			.on('start',function (ev) {
+				console.log('==================\nserver started...\n==================');
+			})
+			.on('restart',function (ev) {
+				console.log('===============\nServer restarted\n===============');
+			})
+			.on('stop',function (ev) {
+				console.log('server stopped');
+			});
 });
