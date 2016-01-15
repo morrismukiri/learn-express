07718812a2951cf7b7c1c12187a0e213041ee8bf
Morris Mukiri <morrismukiri@gmail.com>
2016-01-15 21:21:48 +0300

    Added Products View From Mongo Collection

--- routes/products.js
+++ routes/products.js
@@ -1,14 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var mongodb = require('mongodb').MongoClient;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a useful resource');
+router.get('/', function (req, res, next) {
+    var url = 'mongodb://localhost:27017/learnExpress';
+    mongodb.connect(url, function (err, db) {
+        if (err) {
+            console.log(err);
+        } else {
+            var collection = db.collection('products');
+            collection.find({}).toArray(function (err, results) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.render('products/index',
+                        {
+                            products: results
+                        }
+                        )
+                }
+
+            })
+        }
+
+    })
+    // res.send('respond with a useful resource');
 });
-router.get('/edit/:id', function(req, res, next) {
-  res.send('Edit Product');
+router.get('/edit/:id', function (req, res, next) {
+
+    res.send('Edit Product');
 });
-router.get('/add', function(req, res, next) {
-  res.send('Add Product');
+router.get('/add', function (req, res, next) {
+    res.send('Add Product');
 });
 module.exports = router;
