6d6aaa3db2056da46a0fae35e17788b4eefa4617
Morris Mukiri <morrismukiri@gmail.com>
2016-01-20 00:11:47 +0300

    Products add to collection

--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var mongodb = require('mongodb').MongoClient;
-// var bodyParser = require('body-parser');
-// var multer = require('multer'); // v1.0.5
-// var upload = multer(); // for parsing multipart/form-data
-
+var url = 'mongodb://localhost:27017/learnExpress';
+// router.all(function (req, res, next) {
+//     var url = 'mongodb://localhost:27017/learnExpress';
+//     mongodb.connect(url, function (err, db) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             req.collection = db.collection('products');
+//             next();
+//         }
+//     });
+    
+// });
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    var url = 'mongodb://localhost:27017/learnExpress';
     mongodb.connect(url, function (err, db) {
         if (err) {
             console.log(err);
@@ -26,25 +34,31 @@ router.get('/', function (req, res, next) {
 
             });
         }
-
     });
-    // res.send('respond with a useful resource');
+
 });
 router.get('/edit/:id', function (req, res, next) {
 
     res.send('Edit Product');
 });
 router.get('/add', function (req, res, next) {
-    res.render('products/add',{
-        title:"Add Product"
+    res.render('products/add', {
+        title: "Add Product"
     });
 });
 router.post('/add', function (req, res, next) {
-    console.log('-------');
-    // console.log(req);
-    var name = req.body.name;
-    res.json(req.body);
-    console.log(name);
-    // res.render('products/add');
+    //Very bad idea. Move this to middleware
+    mongodb.connect(url, function (err, db) {
+        if (err) {
+            console.log(err);
+        } else {
+            var collection = db.collection('products');
+            collection.insert({ name: req.body.name, price: req.body.price });
+            res.redirect(303,'/products');
+        }
+    });
+
+
+    // res.render('products/index', { title: 'Products', message: 'Product Added' });
 });
 module.exports = router;
